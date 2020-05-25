e0b1d5da172c6666a06e0497882221a39e0e57fd
Jesse Hix <jesse.hix@locationlabs.com>
2020-05-24 23:25:41 -0700

    The same thing we do every night, Pinky - try to take over the world!

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+	public float speed = 30;
+
+	void FixedUpdate() {
+        float verticalPress = Input.GetAxisRaw ("Vertical");
+        float horizontalPress = Input.GetAxisRaw ("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalPress, verticalPress) * speed;
+	}
+
+    // Start is called before the first frame update
+    void Start() {}
+
+    // Update is called once per frame
+    void Update() {}
+}
